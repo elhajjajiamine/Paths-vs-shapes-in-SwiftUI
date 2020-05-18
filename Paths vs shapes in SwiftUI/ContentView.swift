3265fbb9857c1e294dd9510259eb98376debc965
elhajjaji <elhajjajiadiba@gmail.com>
2020-05-18 13:02:39 +0000

    Initial Commit

---
+++ Paths vs shapes in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Paths vs shapes in SwiftUI
+//
+//  Created by Elhajjaji on 5/18/20.
+//  Copyright © 2020 Elhajjaji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
