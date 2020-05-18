57436814b09d7db14e38be56df6c601b2d0f32f6
elhajjaji <elhajjajiadiba@gmail.com>
2020-05-18 13:48:56 +0000

    Paths vs shapes in SwiftUI

--- Paths vs shapes in SwiftUI/ContentView.swift
+++ Paths vs shapes in SwiftUI/ContentView.swift
@@ -8,9 +8,47 @@
 
 import SwiftUI
 
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+
+        return path
+    }
+}
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Triangle()
+            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 300, height: 300)
+            
+            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .stroke(Color.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
+
+
+        }
     }
 }
 
